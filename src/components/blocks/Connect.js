a68ec4ba68b200f59cded982dd5a8884b197c646
Stanley Pageot <stanley@rxdu.io>
2022-09-20 16:18:09 -0700

    feat: added import info

--- src/components/blocks/Connect.js
+++ src/components/blocks/Connect.js
@@ -20,19 +20,28 @@ import {
 import { validateUrl } from '../../utils/urlUtils';
 
 const ConnectContainer = styled('button')`
-  width: 300px;
-  height: 50px;
+  width: 18.75rem;
+  height: 3.125rem;
   align-self: center;
   background: none;
   color: black;
-  border: 1px solid black;
-  border-radius: 10px;
+  border: 0.0625rem solid black;
+  border-radius: 0.625rem;
+`;
+
+const StyledContainer = styled('div')`
+  height: 12.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Connect = () => {
   const intl = useIntl();
   const [isConnectModalOpen, setIsConnectModalOpen] = useState(false);
-  const [apiKey, setApiKey] = useState('');
+  const [apiKey, setApiKey] = useState(null);
+  const [orgUid, setOrgUid] = useState('');
   const [serverAddress, setServerAddress] = useState('');
   const [tabValue, setTabValue] = useState(0);
 
@@ -61,53 +70,75 @@ const Connect = () => {
           body={
             <ModalFormContainerStyle>
               <TabPanel value={tabValue} index={0}>
-                <StyledFieldContainer>
-                  <StyledLabelContainer>
-                    <Body>
-                      *<FormattedMessage id="server-address" />
-                    </Body>
-                  </StyledLabelContainer>
-                  <InputContainer>
-                    <StandardInput
-                      size={InputVariantEnum.large}
-                      variant={InputVariantEnum.default}
-                      value={serverAddress}
-                      onChange={value => setServerAddress(value)}
-                      placeholderText="http://0.0.0.0:31310"
-                    />
-                  </InputContainer>
-                  {(serverAddress === null ||
-                    validateUrl(serverAddress) === false) && (
-                    <Body size="Small" color="red">
-                      {intl.formatMessage({
-                        id: 'add-valid-server-address',
-                      })}
-                    </Body>
-                  )}
-                </StyledFieldContainer>
-                <StyledFieldContainer>
-                  <StyledLabelContainer>
-                    <Body>
-                      *<FormattedMessage id="api-key" />
-                    </Body>
-                  </StyledLabelContainer>
-                  <InputContainer>
-                    <StandardInput
-                      size={InputSizeEnum.large}
-                      variant={InputVariantEnum.default}
-                      value={apiKey}
-                      onChange={value => setApiKey(value)}
-                      placeholderText="xxxxxxx-xxxxxx-xxxxxx"
-                    />
-                  </InputContainer>
-                  {apiKey === null && (
-                    <Body size="Small" color="red">
-                      {intl.formatMessage({
-                        id: 'add-valid-api-key',
-                      })}
-                    </Body>
-                  )}
-                </StyledFieldContainer>
+                <StyledContainer>
+                  <StyledFieldContainer>
+                    <StyledLabelContainer>
+                      <Body>
+                        *<FormattedMessage id="server-address" />
+                      </Body>
+                    </StyledLabelContainer>
+                    <InputContainer>
+                      <StandardInput
+                        size={InputVariantEnum.large}
+                        variant={InputVariantEnum.default}
+                        value={serverAddress}
+                        onChange={value => setServerAddress(value)}
+                        placeholderText="http://0.0.0.0:31310"
+                      />
+                    </InputContainer>
+                    {(serverAddress === null ||
+                      validateUrl(serverAddress) === false) && (
+                      <Body size="Small" color="red">
+                        {intl.formatMessage({
+                          id: 'add-valid-server-address',
+                        })}
+                      </Body>
+                    )}
+                  </StyledFieldContainer>
+                  <StyledFieldContainer>
+                    <StyledLabelContainer>
+                      <Body>
+                        *<FormattedMessage id="api-key" />
+                      </Body>
+                    </StyledLabelContainer>
+                    <InputContainer>
+                      <StandardInput
+                        size={InputSizeEnum.large}
+                        variant={InputVariantEnum.default}
+                        value={apiKey}
+                        onChange={value => setApiKey(value)}
+                        placeholderText="xxxxxxx-xxxxxx-xxxxxx"
+                      />
+                    </InputContainer>
+                    {apiKey === null && (
+                      <Body size="Small" color="red">
+                        {intl.formatMessage({
+                          id: 'add-valid-api-key',
+                        })}
+                      </Body>
+                    )}
+                  </StyledFieldContainer>
+                </StyledContainer>
+              </TabPanel>
+              <TabPanel value={tabValue} index={1}>
+                <StyledContainer>
+                  <StyledFieldContainer>
+                    <StyledLabelContainer>
+                      <Body>
+                        *<FormattedMessage id="org-uid" />
+                      </Body>
+                    </StyledLabelContainer>
+                    <InputContainer>
+                      <StandardInput
+                        size={InputSizeEnum.large}
+                        variant={InputVariantEnum.default}
+                        value={orgUid}
+                        onChange={value => setOrgUid(value)}
+                        placeholderText="Org Uid"
+                      />
+                    </InputContainer>
+                  </StyledFieldContainer>
+                </StyledContainer>
               </TabPanel>
             </ModalFormContainerStyle>
           }
