d6651d9df3b34b52046cf320cff6c7e8b2c8d1e9
Daniel Stefan <daniel.stefan@equilobe.com>
2022-10-03 18:58:06 +0300

    feat: update hosted API_HOST in constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,5 @@
 import _ from 'lodash';
 
-const hostName = String(_.get(window, 'location.hostname', ''));
-
 export default {
   API_HOST: 'http://localhost:31311',
   HEADER_HEIGHT: 64, // Needed to be used to calculate max height for body components,
