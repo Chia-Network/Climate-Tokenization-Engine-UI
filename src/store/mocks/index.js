70a383a81883063067ca68f3cfbd7304e32fde52
Daniel Stefan <daniel.stefan@equilobe.com>
2022-10-10 14:44:41 +0300

    feat: infer project name and display in table

--- src/store/mocks/index.js
+++ src/store/mocks/index.js
@@ -1,2 +1,3 @@
 export * as mockedExampleResponse from './example.json';
-export * as getUntokenizedUnitsStub from './untokenized-units.json';
+export * as untokenizedUnitsStub from './untokenized-units.json';
+export * as projectsStub from './projects.json';
