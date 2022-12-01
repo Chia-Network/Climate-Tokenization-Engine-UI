6448a1cb667137a07fa0206c4983ddfedd1dbc75
Daniel Stefan <daniel.stefan@equilobe.com>
2022-12-01 16:03:45 +0200

    feat: add about window

--- src/start.js
+++ src/start.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu, MenuItem } = require('electron');
+const openAboutWindow = require('about-window').default;
 
 const path = require('path');
 const url = require('url');
@@ -34,7 +35,42 @@ function createWindow() {
     e.preventDefault();
     require('electron').shell.openExternal(url);
   });
+
+  let defaultMenu = Menu.getApplicationMenu();
+  let newMenu = new Menu();
+  defaultMenu.items.forEach(mainMenuItem => {
+    newMenu.append(mainMenuItem);
+  });
+
+  newMenu.append(
+    new MenuItem({
+      label: 'About',
+      submenu: [
+        {
+          label: 'About',
+          click() {
+            openAboutWindow({
+              icon_path: path.join(
+                __dirname,
+                '/../public/android-chrome-512x512.png',
+              ),
+              product_name: 'Tokenization Engine',
+              description: 'The Tokenization Engine is about lorem ipsum ...',
+              copyright: 'Copyright (c) 2022 Chia Network 2022',
+              app,
+              BrowserWindow,
+              ipcMain,
+            });
+          },
+        },
+      ],
+    }),
+  );
+
+  Menu.setApplicationMenu(newMenu);
 }
+
+// app.applicationMenu = menu;
 
 app.on('ready', createWindow);
 
@@ -49,3 +85,56 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+/*
+openAboutWindow({
+  icon_path: path.join(__dirname, '/../public/favicon.ico'),
+  app,
+  BrowserWindow,
+  ipcMain,
+});
+
+Menu.buildFromTemplate([
+  {
+    label: 'About',
+    submenu: [
+      // {
+      //   label: 'About This App',
+      //   click: () =>
+      //     openAboutWindow({
+      //       icon_path: join(__dirname, 'icon.png'),
+      //       copyright: 'Copyright (c) 2015 rhysd',
+      //       package_json_dir: __dirname,
+      //       open_devtools: process.env.NODE_ENV !== 'production',
+      //     }),
+      // },
+      // {
+      //   label: 'About This App (custom version entry)',
+      //   click: () =>
+      //     openAboutWindow({
+      //       icon_path: join(__dirname, 'icon.png'),
+      //       copyright: 'Copyright (c) 2015 rhysd',
+      //       package_json_dir: __dirname,
+      //       use_version_info: [
+      //         ['my version entry 1', 'a.b.c'],
+      //         ['my version entry 2', 'x.y.z'],
+      //       ],
+      //     }),
+      // },
+      {
+        label: 'About This App (modal with close)',
+        click: () =>
+          openAboutWindow({
+            icon_path: '../public/favicon.ico',
+            app,
+            BrowserWindow,
+            ipcMain,
+          }),
+      },
+      {
+        role: 'quit',
+      },
+    ],
+  },
+]);
+*/
